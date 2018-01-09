628ef1439ae1bdd707c280a6bd842e4931f423d0
Reginaldo Junior <76regi@gmail.com>
2018-01-09 11:46:59 -0200

    Update views.py

--- spotify_indicacoes/views.py
+++ spotify_indicacoes/views.py
@@ -4,8 +4,8 @@ import spotipy
 from spotipy import oauth2
 
 PORT_NUMBER = 8000
-SPOTIPY_CLIENT_ID = '126336c473d64c0ba869e44b7c0ec8b7'
-SPOTIPY_CLIENT_SECRET = 'e5398244b9a842018d401754397e4f43'
+SPOTIPY_CLIENT_ID = ''
+SPOTIPY_CLIENT_SECRET = ''
 SPOTIPY_REDIRECT_URI = 'http://localhost:8000'
 SCOPE = 'user-library-read'
 CACHE = '.spotipyoauthcache'
