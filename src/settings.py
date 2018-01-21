2978f53530817e76a7fe6e52bad5dab2f10bb16e
Reginaldo Junior <76regi@gmail.com>
2018-01-20 23:31:08 -0200

    remove sensitive data

--- src/settings.py
+++ src/settings.py
@@ -81,9 +81,9 @@ WSGI_APPLICATION = 'src.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', 
-        'NAME': 'indicate_songs',
-        'USER': 'root',
-        'PASSWORD': 'root',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
         'PORT': '3306',
     }
